bbe84c71875a9ccbc34ea557e5dff651bcb959f2
Alexander Groden <alexander.groden@gmail.com>
2014-02-04 18:37:04 -0500

    no longer uses bozorth3 and handles bad input better

--- src/drptool/main.cpp
+++ src/drptool/main.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include "drp.h"
 
+
 void print_usage() {
 	std::cout << "drptool [options] <infile>.xyt <outfile>.drp\n";
 	std::cout << "drptool converts .xyt files to .drp files\n";
@@ -20,22 +21,39 @@ void print_usage() {
 	std::cout << "\t-n #\tset the number of neighbors (default 6)\n";
 }
 
+
+int err_exit(std::string err) {
+	std::cerr << "Error: " << err << "\n";
+	std::cerr << "use -h for help" << std::endl;
+	return -1;
+}
+
+
 int main(int argc, const char* argv[]) {
-	if (argc < 3)
-		print_usage();
 	std::vector<const char*> positional_args;
 	unsigned int n = 6;
-	for (int i = 0; i < argc; ++i) {
+	for (int i = 1; i < argc; ++i) {
 		std::string s(argv[i]);
-		if ("-n" == s)
-			n = std::stoi(argv[i + 1]);
-		else if ("-h" == s) {
-			print_usage();
-			break;
+		if (s[0] == '-') {
+			if ("-n" == s) {
+				if (!argv[i+1]) {
+					return err_exit("-n flag requires an argument");
+				}
+				n = std::stoi(argv[i + 1]);
+			} else if ("-h" == s) {
+				print_usage();
+				return 0;
+			} else {
+				return err_exit("invalid option");
+			}
 		} else {
 			positional_args.push_back(argv[i]);
 		}
 	}
+	if (positional_args.size() != 2) {
+		return err_exit("incorrect number of arguments");
+	}
+
 	const drp::drp_t* drp = drp::load_xyt(positional_args[0], n);
 	drp::save(positional_args[1], drp);
 	return 0;
