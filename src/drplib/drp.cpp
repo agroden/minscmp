99053f387748da6b96a02fc2f24f0984c04325cf
Alexander Groden <alexander.groden@gmail.com>
2014-02-04 00:24:03 -0500

    moving away from dependency on bozorth because ARGH NAME MANGLING

--- src/drplib/drp.cpp
+++ src/drplib/drp.cpp
@@ -1,56 +1,114 @@
-#include <math>
+/// drp.cpp
+///
+/// Contains functions for reading and creating records for use with minscmp.
+///
+/// Alexander Groden
+/// created for CYB5677 - Biometric Authentication Technologies
+
+#include <cmath>
+#include <fstream>
 #include "drp.h"
 
-using namespace drp;
-
-const drp_t* convert_xyt(size_t num_neighbors, const xyt_struct* xyt) {
-	for (size_t i = 0; i < xyt->nrows; ++i) {
-		drp_record_t* curr = new drp_record_t(i, xyt->thetacol[i]);
-		// look for nearest neighbors
-		for (size_t j = 0; j < xyt->nrows; ++i) {
-			if (j == i)
-				continue;
-			float d = distance(j, i, xyt);
-			// first pass, don't calculate the tough stuff yet
-			if (curr->neighbors.size() < num_neighbors)
-				curr->neighbors.push_back(new drp_row(j, d));
-			else {
-				for (auto k = curr_group.begin(); k != curr_group.end(); ++k) {
-					if (k->distance > d) {
-						curr->neighbors.erase(k);
-						curr->neighbors.push_back(new drp_row(j, d));
-						delete k;
-						break;
+namespace drp {
+	// forward declare file only convenience functions
+	static float distance(size_t n_id, size_t m_id, const xyt_struct* xyt);
+	static float radial_angle(size_t n_id, size_t m_id, const xyt_struct* xyt);
+	static float positional_angle(size_t n_id, size_t m_id, const xyt_struct* xyt);
+	
+	const drp_t* convert_xyt(const xyt_struct* xyt, size_t num_neighbors) {
+		drp_t* drp = new drp_t();
+		for (int i = 0; i < xyt->nrows; ++i) {
+			drp_record* curr = new drp_record(i, xyt->thetacol[i]);
+			drp_group_t* group = curr->neighbors();
+			// look for nearest neighbors
+			for (int j = 0; j < xyt->nrows; ++i) {
+				if (j == i)
+					continue;
+				float d = distance(j, i, xyt);
+				// first pass, don't calculate the tough stuff yet
+				if (group->size() < num_neighbors)
+					group->push_back(drp_row_ptr(new drp_row(j, d)));
+				else {
+					for (auto k = group->begin(); k != group->end(); ++k) {
+						if ((*k)->distance() > d) {
+							group->erase(k);
+							group->push_back(drp_row_ptr(new drp_row(j, d)));
+							break;
+						}
 					}
 				}
 			}
+			// calculate the rest of the info for the local group
+			for (auto n = group->begin(); n != group->end(); ++n) {
+				(*n)->radial_angle(radial_angle((*n)->id(), i, xyt));
+				(*n)->positional_angle(positional_angle((*n)->id(), i, xyt));
+			}
+			drp->push_back(drp_record_ptr(curr));
 		}
-		// calculate the rest of the info for the local group
-		for (auto n = curr->neighbors.begin(); n != curr->neighbors.end(); ++n) {
-			n->radial_angle = radial_angle(n->minutiae_id, i, xyt);
-			n->positional_angle = positional_angle(n->minutiae_id, i, xyt);
-		}
-		m_groups.push_back(curr);
+		return drp;
 	}
-}
 
-const drp_t* drp::from_file(size_t num_neighbors, const char* path) {
-	xyt_struct xyt;
-	// TODO: fix this biz
-	//bz_load(path, &xyt);
-	return convert_xyt(num_neighbors, &xyt);
-}
 
-static float drp::distance(size_t n_id, size_t m_id, xyt_struct* xyt) {
-	return sqrt(pow((xyt->xcol[n_id] - xyt->xcol[m_id]), 2) + 
-				pow((xyt->ycol[n_id] - xyt->ycol[m_id]), 2));
-}
+	const drp_t* load_xyt(const char* path, size_t num_neighbors) {
+		xyt_struct* xyt = bz_load(path);
+		const drp_t* ret = convert_xyt(xyt, num_neighbors);
+		delete xyt;
+		return ret;
+	}
 
-static float drp::radial_angle(size_t n_id, size_t m_id, xyt_struct* xyt) {
-	return xyt->thetacol[n->minutiae_id] - xyt->thetacol[i];
-}
 
-static float drp::positional_angle(size_t n_id, size_t m_id, xyt_struct* xyt) {
-	return atan((xyt->ycol[n_id] - xyt->ycol[m_id]) / 
-				(xyt->xcol[n_id] - xyt->xcol[m_id])) - xyt->thetacol[i];
+	void save(const char* path, const drp_t* drp) {
+		std::ofstream ofile(path, std::ofstream::out);
+		for (auto i = drp->begin(); i != drp->end(); ++i) {
+			drp_group_t* group = (*i)->neighbors();
+			ofile << (*i)->id() << " ";
+			ofile << (*i)->ridge_direction() << " ";
+			ofile << group->size() << "\n";
+			for (auto j = group->begin(); j != group->end(); ++i) {
+				ofile << (*j)->id() << " ";
+				ofile << (*j)->distance() << " ";
+				ofile << (*j)->radial_angle() << " ";
+				ofile << (*j)->positional_angle() << "\n";
+			}
+		}
+		ofile.close();
+	}
+
+
+	const drp_t* load(const char* path) {
+		std::ifstream ifile(path, std::ifstream::in);
+		drp_t* drp = new drp_t();
+		unsigned int id;
+		float rd;
+		size_t n;
+		while (ifile >> id >> rd >> n) {
+			drp_record* curr = new drp_record(id, rd);
+			drp_group_t* group = curr->neighbors();
+			drp->push_back(drp_record_ptr(curr));
+			float d, ra, pa;
+			for (size_t i = 0; i < n; ++i) {
+				ifile >> id >> d >> ra >> pa;
+				group->push_back(drp_row_ptr(new drp_row(id, d, ra, pa)));
+			}
+		}
+		ifile.close();
+		return drp;
+	}
+
+
+	static float distance(size_t n_id, size_t m_id, const xyt_struct* xyt) {
+		return sqrt(pow((xyt->xcol[n_id] - xyt->xcol[m_id]), 2) + 
+					pow((xyt->ycol[n_id] - xyt->ycol[m_id]), 2));
+	}
+
+
+	static float radial_angle(size_t n_id, size_t m_id, const xyt_struct* xyt) {
+		return xyt->thetacol[n_id] - xyt->thetacol[m_id];
+	}
+
+
+	static float positional_angle(size_t n_id, size_t m_id, const xyt_struct* xyt) {
+		return atan((xyt->ycol[n_id] - xyt->ycol[m_id]) / 
+					(xyt->xcol[n_id] - xyt->xcol[m_id])) - xyt->thetacol[m_id];
+	}
 }
--- src/drplib/drp.h
+++ src/drplib/drp.h
@@ -11,38 +11,61 @@
 #include <deque>
 #include <memory>
 #include <string>
-#include "bozorth.h"
 
 namespace drp {
-	typedef struct drp_row {
-		size_t minutia_id;
-		float distance;
-		float radial_angle;
-		float positional_angle;
-		drp_row(size_t id, float d, float ra = 0, float pa = 0) : 
-			minutiae_id(id), distance(d), radial_angle(ra), 
-			positional_angle(pa) { }
-	} drp_row_t;
+	class minutiae {
+	public:
+		minutiae(unsigned int id) : m_id(id) { }
+		~minutiae() { }
+		unsigned int id() const { return m_id; }
+	private:
+		unsigned int m_id;
+	};
 
-	typedef std::deque<std::unique_ptr<drp_row_t>> drp_group_t;
+	class drp_row : public minutiae {
+	public:
+		drp_row(unsigned int id, float d, float ra = 0, float pa = 0) :
+			minutiae(id), m_distance(d), m_radial_angle(ra),
+			m_positional_angle(pa) { }
+		~drp_row() { }
+		float distance() const { return m_distance; }
+		float radial_angle() const { return m_radial_angle; }
+		void radial_angle(float ra) { m_radial_angle = ra; }
+		float positional_angle() const { return m_positional_angle; }
+		void positional_angle(float pa) { m_positional_angle = pa; }
+	private:
+		float m_distance;
+		float m_radial_angle;
+		float m_positional_angle;
+	};
 
-	typedef struct drp_record {
-		size_t minutiae_id;
-		drp_group_t neighbors;
-		float ridge_direction;
-		drp_record(size_t id, float rd) : 
-			minutiae_id(id), neighbors(grp), ridge_direction(rd) { }
-		~drp_record() { neighbors.clear(); }
-	} drp_record_t;
+	typedef std::unique_ptr<drp_row> drp_row_ptr;
+	typedef std::deque<drp_row_ptr> drp_group_t;
 
-	typedef std::deque<std::unique_ptr<drp_record_t>> drp_t;
+	class drp_record : public minutiae {
+	public:
+		drp_record(unsigned int id, float rd) : minutiae(id), 
+			m_ridge_direction(rd) { }
+		~drp_record() { m_neighbors.clear(); }
+		drp_group_t* neighbors() { return &m_neighbors; }
+		float ridge_direction() { return m_ridge_direction; }
+	private:
+		drp_group_t m_neighbors;
+		float m_ridge_direction;
+	};
 
-	const drp_t* convert_xyt(size_t num_neighbors = 6, const xyt_struct* xyt);
-	const drp_t* load(size_t num_neighbors = 6, const char* path);
+	typedef std::unique_ptr<drp_record> drp_record_ptr;
+	typedef std::deque<drp_record_ptr> drp_t;
 
-	static float distance(size_t n_id, size_t m_id, xyt_struct* xyt);
-	static float radial_angle(size_t n_id, size_t m_id, xyt_struct* xyt);
-	static float positional_angle(size_t n_id, size_t m_id, xyt_struct* xyt);
+	const drp_t* convert_xyt(const xyt_t* xyt, size_t num_neighbors = 6);
+	const drp_t* load_xyt(const char* path, size_t num_neighbors = 6);
+
+	// file format:
+	// minutiae_id ridge_direction number_of_neighbors
+	// minutiae_id distance radial_angle positional_angle
+	// ... (repeats for number_of_neighbors)
+	void save(const char* path, const drp_t* drp);
+	const drp_t* load(const char* path);
 }
 
 #endif
